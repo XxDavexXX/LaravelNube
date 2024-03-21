f682311d072d15e09cd38c88af4bb44e9837e720
XxDavexX <dave.santivanez@gmail.com>
2024-03-21 11:08:32 -0500

    avance

---
+++ webjf02/resources/views/index.blade.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Imagen de Real Madrid</title>
+</head>
+<body style="margin:0;">
+    <img style="width:100%;" src="{{ asset('img/realmadrid.jpeg') }}" alt="Mi imagen">
+</body>
+</html>
